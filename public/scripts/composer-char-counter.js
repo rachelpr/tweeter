10e7ae8468661fce7d9731ca91ec199729b06221
Rachel PR <rplourder@gmail.com>
2022-03-16 17:41:56 -0300

    Add functionality for character count in textarea

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+// function to track key events in the textarea
+// uses charCount to show how many characters are left in the output tag
+$(document).ready(function () {
+  $("textarea").keyup(function () {
+    let $tweet = $(this).val();
+    let $charCount = 140 - $tweet.length;
+    let $counter = $(this).parents("form").find(".counter");
+
+    $("output").text($charCount);
+
+    if ($charCount < 0) {
+      $($counter).addClass("counterRed");
+    } else {
+      $($counter).removeClass("counterRed");
+    }
+  });
+});
+
