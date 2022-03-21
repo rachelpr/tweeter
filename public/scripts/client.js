b412b909d4f01fdcd891258e4c7dc50e4e637117
Rachel PR <rplourder@gmail.com>
2022-03-21 20:43:47 -0300

    Add function to load tweets on the page. Remove the testing code and call to renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 // Submitting a new tweet with our form
 // Preventing it from loading on a new page
 $("#submit-tweet").on("submit", function (event) {
@@ -50,7 +24,7 @@ const renderTweets = function (tweets) {
   }
 };
 
-// Function to create the tweet element 
+// Function to create the tweet element
 const createTweetElement = function (tweet) {
   const $tweet = $(`<article class="tweet"></article>`);
   let $tweetContent = $(`
@@ -74,4 +48,16 @@ const createTweetElement = function (tweet) {
   return $tweet.html($tweetContent);
 };
 
-renderTweets(data);
+// Function to load the tweets on the page
+$(document).ready(function () {
+  const loadTweets = function () {
+    let tweet = $("#submit-tweet").serialize();
+    $.ajax("/tweets", {
+      type: "GET",
+      dataType: "JSON",
+    }).then(function (tweets) {
+      renderTweets(tweets);
+    });
+  };
+  loadTweets();
+});
